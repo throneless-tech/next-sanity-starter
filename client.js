e6de25174cf795ea795781cdf985088aa9f01604
rudietuesdays <rae@throneless.tech>
2021-12-02 13:39:49 -0500

    add template starter files

---
+++ client.js
@@ -0,0 +1,9 @@
+const sanityClient = require("@sanity/client");
+
+module.exports = sanityClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+  useCdn: false
+});
